3124701688fac051ed45140752cd95f1b416c43d
Oleg <olegpanin1968@gmail.com>
2024-06-20 15:13:08 +0300

    Initial commit

--- src/app/CalcCostBase.java
+++ src/app/CalcCostBase.java
@@ -1,4 +1,8 @@
 package app;
 
 public class CalcCostBase {
+    // Базовий розрахунок вартості товару
+    public double calcCost(Product product) {
+        return product.getQuota() * product.getPrice(); // correct two typos
+    }
 }
--- src/app/CalcCostDelivery.java
+++ src/app/CalcCostDelivery.java
@@ -1,4 +1,14 @@
 package app;
 
-public class CalcCostDelivery {
+public class CalcCostDelivery extends CalcCostBase {
+    // Вартість доставки
+    private final static double deliveryPrice = 7; // add initialization
+
+    // Розрахунок вартості товару,
+    // з урахуванням вартості доставки
+    @Override
+    public double calcCost(Product product) {
+        return product.getQuota() * product.getPrice()  // correct a typo
+                + deliveryPrice;
+    }
 }
